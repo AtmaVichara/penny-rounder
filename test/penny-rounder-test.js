1f85a16cedbc1452e12b4fa584d68226658c3948
Joe Jobes <jmrjobes@gmail.com>
2018-06-29 10:03:51 -0600

    Add penny-rounder tests. Add test for totalSplitter(), verifySplit(), and moneySplitter()

---
+++ test/penny-rounder-test.js
@@ -0,0 +1,29 @@
+const PennyRounder = require("../lib/penny-rounder.js")
+const assert = require("chai").assert;
+
+describe("Penny Rounder Test", () => {
+
+  describe("Methods", () => {
+    describe("totalSplitter() functionality", () => {
+      it("should split total into three numbers, and return array of split", () => {
+        assert.deepEqual(PennyRounder.totalSplitter(800.00), [266.67, 266.67, 266.67])
+        assert.deepEqual(PennyRounder.totalSplitter(850.00), [283.33, 283.33, 283.33])
+      })
+    })
+
+    describe("verifySplit() functionality", () => {
+      it("should add/subtract values until they add up to total, then return", () => {
+        assert.deepEqual(PennyRounder.verifySplit([266.67, 266.67, 266.67], 800.00), [266.66, 266.67, 266.67])
+        assert.deepEqual(PennyRounder.verifySplit([283.33, 283.33, 283.33], 850.00), [283.34, 283.33, 283.33])
+      })
+    })
+
+    describe("moneySplitter() functionality", () => {
+      it("should split total and return array of split values", () => {
+        assert.deepEqual(PennyRounder.moneySplitter(800.00), [266.66, 266.67, 266.67])
+        assert.deepEqual(PennyRounder.moneySplitter(850.00), [283.34, 283.33, 283.33])
+      })
+    })
+  })
+
+})
