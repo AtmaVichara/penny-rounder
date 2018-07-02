65242d0663da902e91b2bf1340d020b02eb892a4
Joe Jobes <jmrjobes@gmail.com>
2018-07-02 06:54:55 -0600

    Add count to penny-rounder with updated comments and tests

--- test/penny-rounder-test.js
+++ test/penny-rounder-test.js
@@ -7,8 +7,10 @@ describe("Penny Rounder Test", () => {
 
     describe("totalSplitter() functionality", () => {
       it("should split total into three numbers, and return array of split", () => {
-        assert.deepEqual(PennyRounder.totalSplitter(800.00), [266.67, 266.67, 266.67])
-        assert.deepEqual(PennyRounder.totalSplitter(850.00), [283.33, 283.33, 283.33])
+        assert.deepEqual(PennyRounder.totalSplitter(800.00, 3), [266.67, 266.67, 266.67])
+        assert.deepEqual(PennyRounder.totalSplitter(850.00, 3), [283.33, 283.33, 283.33])
+        assert.deepEqual(PennyRounder.moneySplitter(740.00, 4), [185, 185, 185, 185])
+        assert.deepEqual(PennyRounder.moneySplitter(1223.00, 4), [305.75, 305.75, 305.75, 305.75])
       })
     })
 
@@ -21,9 +23,10 @@ describe("Penny Rounder Test", () => {
 
     describe("moneySplitter() functionality", () => {
       it("should split total and return array of split values", () => {
-        assert.deepEqual(PennyRounder.moneySplitter(800.00), [266.66, 266.67, 266.67])
-        assert.deepEqual(PennyRounder.moneySplitter(850.00), [283.34, 283.33, 283.33])
-        assert.deepEqual(PennyRounder.moneySplitter(740.00), [411.33, 411.33, 411.33])
+        assert.deepEqual(PennyRounder.moneySplitter(800.00, 3), [266.66, 266.67, 266.67])
+        assert.deepEqual(PennyRounder.moneySplitter(850.00, 3), [283.34, 283.33, 283.33])
+        assert.deepEqual(PennyRounder.moneySplitter(740.00, 4), [185, 185, 185, 185])
+        assert.deepEqual(PennyRounder.moneySplitter(1223.00, 4), [305.75, 305.75, 305.75, 305.75])
       })
     })
   })
